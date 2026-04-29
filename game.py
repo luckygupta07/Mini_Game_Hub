3bddc97c5ab52f6a261a072154b75a09894a516c
luckygupta07 <luckygupta16108@gmail.com>
2026-04-29 21:42:58 +0530

    Complete game.py and images

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ from datetime import date
 GAME_MODULES = {
     "Tic-Tac-Toe": ("games.tictactoe",  "TicTacToe"),
     "Othello":      ("games.othello",   "Othello"),
-    "Connect Four": ("games.connect4",  "ConnectFour"),
+    "ConnectFour": ("games.connect4",  "ConnectFour"),
 }
 
 # ---------------------------------------------------------------------------
@@ -40,6 +40,10 @@ def record_result(winner_name: str, loser_name: str, game_name: str):
         writer = csv.writer(f)
         writer.writerow([winner_name, loser_name, date.today().isoformat(), game_name])
 # ---------------------------------------------------------------------------
+def show_leaderboard(sort_by):
+    #sort_by can be 'wins', 'losses', or 'ratio'.
+    subprocess.run(["bash", LEADERBOARD_SH, sort_by])
+
 
 # ---------------------------------------------------------------------------
 #Base class for 2-player turn-based board games
@@ -81,12 +85,8 @@ class BoardGame:
             return "It's a draw!"
         return f"{self.player_names[self.winner]} wins!"
 
-    def get_font(self, size: int, bold: bool = False) -> pygame.font.Font:  ####
-        return pygame.font.SysFont("segoeui", size, bold=bold)
-
-    def draw_something(self,surf: pygame.Surface):
-        
-        pass
+    def get_font(self, size: int, bold: bool=False) -> pygame.font.Font:  ####
+        return pygame.font.SysFont("segoeui", size, bold)
 
 
 # ===========================================================================
@@ -99,7 +99,7 @@ class MenuScreen:
     DESCRIPTIONS = {
         "Tic-Tac-Toe": "10 × 10  ·  5 in a row to win",
         "Othello":      "8 × 8   ·  Flip your opponent's discs",
-        "Connect Four": "7 × 7   ·  Drop balls, connect 4",
+        "ConnectFour": "7 × 7   ·  Drop balls, connect 4",
     }
 
     def __init__(self, screen: pygame.Surface, p1: str, p2: str):
@@ -113,11 +113,21 @@ class MenuScreen:
         first_row_y   = 320
         self.row_tops = [first_row_y + i * (self.row_h + 16) for i in range(len(self.GAMES))]
 
-        #Font styles
-        self.f_title = pygame.font.SysFont("segoeui", 60, bold=True)
-        self.f_sub   = pygame.font.SysFont("segoeui", 50)
-        self.f_game  = pygame.font.SysFont("segoeui", 30, bold=True) ####
-        self.f_hint  = pygame.font.SysFont("segoeui", 15)
+        
+        #Images 
+        self.bg_image = pygame.image.load("images/menu.png")   # replace with your file path
+        self.bg_image = pygame.transform.scale(self.bg_image, (W, H))
+
+        self.c4_image = pygame.image.load("images/c4.png")
+        self.c4_image = pygame.transform.smoothscale(self.c4_image, (self.row_h, self.row_h))
+
+        self.tic_image = pygame.image.load("images/tic.png")
+        self.tic_image = pygame.transform.smoothscale(self.tic_image, (self.row_h, self.row_h))
+
+        self.ot_image = pygame.image.load("images/ot.png")
+        self.ot_image = pygame.transform.smoothscale(self.ot_image, (self.row_h, self.row_h))
+
+
 
     def run(self) -> str:
         clock = pygame.time.Clock()
@@ -145,21 +155,21 @@ class MenuScreen:
             if self.row_rect(i).collidepoint(mx, my):
                 return i
         return -1
+    
+    def get_font(self, size: int, bold: bool = False) -> pygame.font.Font:  ####
+        return pygame.font.SysFont("segoeui", size, bold)
 
     def draw_something(self, mx: int, my: int):
         surf = self.screen
-        surf.fill((10,10,20))
+        
+        surf.blit(self.bg_image, (0, 0))
 
-        #Title
-        title = self.f_title.render("MINI  GAME  HUB", True, (220, 220, 235))
-        surf.blit(title, (W // 2 - title.get_width() // 2, 80))
-
-        p1s = self.f_sub.render(self.p1, True, (255, 90, 90))
-        vs = self.f_sub.render("  vs  ", True, (110, 110, 140))
-        p2s = self.f_sub.render(self.p2, True, (90, 180, 255))
+        p1s = self.get_font(50).render(self.p1, True, (255, 90, 90))
+        vs = self.get_font(50).render("  vs  ", True, (110, 110, 140))
+        p2s = self.get_font(50).render(self.p2, True, (90, 180, 255))
         total = p1s.get_width() + vs.get_width() + p2s.get_width()
         x = W//2 - total // 2
-        y = 150
+        y = 160
         surf.blit(p1s, (x, y)); x += p1s.get_width()
         surf.blit(vs, (x, y)); x += vs.get_width()
         surf.blit(p2s, (x, y))
@@ -167,7 +177,7 @@ class MenuScreen:
         pygame.draw.line(surf, (45, 45, 75),
                          (W // 2 - 150, 210), (W // 2 + 150, 210), 1)
 
-        sel_label = self.f_hint.render("Select a game to play", True, (90, 90, 120))
+        sel_label = self.get_font(15).render("Select a game to play", True, (90, 90, 120))
         surf.blit(sel_label, (W // 2 - sel_label.get_width() // 2, 220))
 
         # Game rows
@@ -182,15 +192,186 @@ class MenuScreen:
                                  (r.x, r.y + 10, 3, r.height - 20),
                                  border_radius=2)
             name_col = (220, 220, 235) if is_hov else (160, 160, 185)
-            surf.blit(self.f_game.render(name, True, name_col),
-                      (r.x + 20, r.y + 14))
-            surf.blit(self.f_hint.render(self.DESCRIPTIONS[name], True, (90, 90, 120)),
-                      (r.x + 20, r.y + 44))
+            surf.blit(self.get_font(30).render(name, True, name_col),
+                      (r.x + 20 + self.row_h, r.y + 20))
+            surf.blit(self.get_font(15).render(self.DESCRIPTIONS[name], True, (90, 90, 120)),
+                      (r.x + 20 + self.row_h, r.y + 50))
+            
+            if i == 0:
+                surf.blit(self.tic_image, (self.row_left, self.row_tops[i]))
+
+            elif i == 1:
+                surf.blit(self.ot_image, (self.row_left, self.row_tops[i]))
+
+            elif i == 2:
+                surf.blit(self.c4_image, (self.row_left, self.row_tops[i]))
+                
             if i < len(self.GAMES) - 1:
                 sep_y = r.bottom + 7
                 pygame.draw.line(surf, (28, 28, 48),
                                  (r.x, sep_y), (r.x + r.w, sep_y), 1)
 
+# ===========================================================================
+# Post-game screen
+# ===========================================================================
+class Postgame:
+
+    SORT_OPTIONS = ["wins", "losses", "ratio"]
+    SORT_LABELS = {
+        "wins":   "Sort by Wins",
+        "losses": "Sort by Losses",
+        "ratio":  "Sort by W/L Ratio",
+    }
+
+    def __init__(self,screen,result_str, winner, loser, game_name):
+        self.screen = screen
+        self.result_str = result_str
+        self.winner = winner
+        self.loser = loser
+        self.game_name = game_name
+
+        self.phase = "sort"
+        #Variables for sort option buttons
+        self.btn_w, self.btn_h = 200, 46
+        self.gap = 20
+        total = len(self.SORT_OPTIONS) * self.btn_w + (len(self.SORT_OPTIONS) - 1) * self.gap
+        self.start_x = W // 2 - total // 2
+        self.btn_y = H // 2 + 60
+        self.btn_x = [self.start_x + i*(self.btn_w+self.gap) for i in range(len(self.SORT_OPTIONS))]
+
+        self.bg_image = pygame.image.load("images/post_game.png")   # replace with your file path
+        self.bg_image = pygame.transform.scale(self.bg_image, (W, H))
+    
+
+    def run(self):
+        clock = pygame.time.Clock()
+        while True:
+            mx, my = pygame.mouse.get_pos()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+                
+                if event.type == pygame.KEYDOWN:
+                    if self._phase == "again":
+                        if event.key == pygame.K_y:
+                            return True
+                        if event.key in (pygame.K_n, pygame.K_ESCAPE):
+                            return False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.phase == "sort":
+                        idx = self.handle_sort_click(mx, my)
+                        if idx >=0 :
+                            self.handle_sort_chosen(idx)
+                    
+                    elif self.phase == "again":
+                        pick = self.handle_again(mx, my)
+                        if pick == "Yes":
+                            return True
+                        elif pick == "No":
+                            return False
+
+            self.draw_bg(self.screen)
+            pygame.display.update()
+            clock.tick(60)
+
+
+
+    def sort_options_rect(self, idx):
+        return pygame.Rect(self.btn_x[idx], self.btn_y, self.btn_w, self.btn_h)
+    
+    def play_again_rect(self):
+        return pygame.Rect(W // 2 - 215, H // 2 + 60, 190, 50)
+     
+    def quit_rect(self):
+        return pygame.Rect(W // 2 + 25, H // 2 + 60, 190, 50)
+
+    def handle_sort_click(self, mx, my):
+        for i in range (len(self.SORT_OPTIONS)):
+            if self.sort_options_rect(i).collidepoint(mx, my):
+                return i
+        return -1
+        
+    def handle_sort_chosen(self, idx):
+        if self.winner is not None:
+            record_result(self.winner,self.loser, self.game_name)
+        else:
+            print("\nGame ended in a draw — no result recorded.\n")
+
+        show_leaderboard(self.SORT_OPTIONS[idx])
+        self.phase = "again"
+
+    def handle_again(self, mx, my):
+        if self.play_again_rect().collidepoint(mx, my):
+            return "Yes"
+        elif self.quit_rect().collidepoint(mx, my):
+            return "No"
+        else :
+            return None
+        
+    def get_font(self, size: int, bold: bool = False) -> pygame.font.Font:  ####
+        return pygame.font.SysFont("segoeui", size, bold=bold)
+
+
+    # ------------------------------------------------------------------
+    # Drawing
+    # ------------------------------------------------------------------
+  
+    def draw_bg(self,surf: pygame.Surface):
+
+        surf.blit(self.bg_image, (0, 0))
+
+    # Central frame
+        rect_w = 660
+        rect_h = 320 if self.phase == "sort" else 260
+        rect_x = W // 2 - rect_w // 2
+        rect_y = H // 2 - rect_h // 2 
+        pygame.draw.rect(surf, ( 28,  28,  50),
+                         (rect_x, rect_y, rect_w, rect_h), border_radius=16)
+        pygame.draw.rect(surf, ( 70,  70, 110),
+                         (rect_x, rect_y, rect_w, rect_h), 1, border_radius=16)
+        
+        res = self.get_font(36, True).render(self.result_str, True, "gold")
+        surf.blit(res, res.get_rect(center=(W // 2, rect_y + 58)))
+
+        pygame.draw.line(surf, ( 70,  70, 110), (rect_x + 30,rect_y + 90), (rect_x + rect_w - 30, rect_y + 90),1)
+
+        if self.phase == "sort":
+            self.draw_sort_phase(surf, rect_y)
+        else :
+            self.draw_again_phase(surf, rect_y)
+
+    def draw_sort_phase(self, surf: pygame.Surface, rect_y):
+        options_text = self.get_font(21, True).render("Choose leaderboard sort metric:", True, "white")
+        surf.blit(options_text, options_text.get_rect(center=(W // 2, rect_y +  120)))
+
+        hint = self.get_font(15).render("Result will be saved and leaderboard printed in terminal", True, (110, 110, 140))
+        surf.blit(hint, hint.get_rect(center=(W // 2, rect_y + 148)))
+
+        for i in range(len(self.SORT_OPTIONS)):
+            r = self.sort_options_rect(i)
+            pygame.draw.rect(surf, ( 50,  50,  80), r, border_radius=10)
+            pygame.draw.rect(surf, ( 70,  70, 110), r, 1, border_radius=10)
+
+            label = self.get_font(16).render(self.SORT_LABELS[self.SORT_OPTIONS[i]], True, (160, 160, 185))
+            surf.blit(label, label.get_rect(center=(r.centerx, r.centery)))
+
+    def draw_again_phase(self, surf: pygame.Surface, rect_y):
+        again_text = self.get_font(21).render("Play another game?", True, "white")
+        surf.blit(again_text, again_text.get_rect(center = (W // 2, rect_y + 120)))
+
+        hint = self.get_font(15).render(
+        "Y  →  back to menu           N / Esc  →  quit", True, (110, 110, 140))
+        surf.blit(hint, hint.get_rect(center = (W // 2, rect_y + 150)))
+
+        for rect, label in [(self.play_again_rect(), "Play Again"), (self.quit_rect(), "Quit")]:
+            pygame.draw.rect(surf, ( 50,  50,  80), rect, border_radius=10)
+            pygame.draw.rect(surf, ( 70,  70, 110), rect, 1, border_radius=10)
+
+            btn_label = self.get_font(16).render(label, True, (160, 160, 185))
+            surf.blit(btn_label, btn_label.get_rect(center = (rect.centerx, rect.centery)))
+                
+        
 
 def load_game_class(game_name: str):
     """Dynamically import and return the game class for the selected game.
@@ -201,6 +382,13 @@ def load_game_class(game_name: str):
     module = importlib.import_module(module_path)
     return getattr(module, class_name)
 
+def start_game(screen: pygame.Surface, game_name: str, p1: str, p2: str):
+
+    GameClass = load_game_class(game_name)
+    game      = GameClass(p1, p2)
+
+    winner, loser = game.run_game(screen)
+    return winner, loser
 
 
 
@@ -227,8 +415,18 @@ def main():
         game_name = menu.run()
 
         if game_name is None:
+            # Window closed from menu
             break
 
+        # Run selected game
+        winner, loser = start_game(screen, game_name, p1, p2)
+
+        result_str = f"{winner} wins!" if winner else "It's a draw!"
+        post = Postgame(screen, result_str, winner, loser, game_name)
+        play_again = post.run()
+
+        if not play_again:
+            break
 
 
     pygame.quit()
